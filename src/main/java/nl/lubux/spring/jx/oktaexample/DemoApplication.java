29fe9c1359c5ca0dc15238298321f781d6263662
Luberto Klaassen <luberto.klaassen@virtualsciences.nl>
2018-08-14 09:10:46 +0200

    Initial import

---
+++ src/main/java/nl/lubux/spring/jx/oktaexample/DemoApplication.java
@@ -0,0 +1,12 @@
+package nl.lubux.spring.jx.oktaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
